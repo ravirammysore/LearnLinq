f8bb7deb07eb1e1c6ba15bc0beacec603059adf2
Ravi K R <raviram.mysore@gmail.com>
2019-01-17 11:31:06 +0530

    groupby experiments

--- Intermediate/IntermForm1.Designer.cs
+++ Intermediate/IntermForm1.Designer.cs
@@ -31,11 +31,15 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(23, 225);
+            this.button1.Location = new System.Drawing.Point(49, 325);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(87, 23);
             this.button1.TabIndex = 2;
@@ -45,7 +49,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(132, 225);
+            this.button2.Location = new System.Drawing.Point(49, 372);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(87, 23);
             this.button2.TabIndex = 3;
@@ -58,20 +62,66 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(39, 29);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(108, 147);
+            this.listBox1.Size = new System.Drawing.Size(108, 264);
             this.listBox1.TabIndex = 4;
             // 
-            // Form1
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(194, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(446, 508);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(49, 470);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "GroupBy() 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_2);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(49, 514);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "GroupBy() 3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(49, 422);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(87, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "GroupBy() 1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // IntermForm1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(841, 560);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "IntermForm1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.IntermForm1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -79,6 +129,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Intermediate/IntermForm1.cs
+++ Intermediate/IntermForm1.cs
@@ -68,16 +68,155 @@ namespace Intermediate
             //using SelectMany()
             var query = petOwners.SelectMany(petOwner => petOwner.Pets);
             listBox1.DataSource = query.ToList();            
+        }     
+
+        List<Pet> pets;
+        private void initPets()
+        {
+            // Create a list of pets.
+            pets =
+                new List<Pet>{ new Pet { Name="Barley", Age=8 },
+                       new Pet { Name="Boots", Age=4 },
+                       new Pet { Name="Whiskers", Age=1 },
+                       new Pet { Name="Daisy", Age=4 } };
+        }
+        //GroupBy() 1
+        private void button5_Click(object sender, EventArgs e)
+        {
+            initPets();
+            // Group the pets using Age as the key value 
+            // and selecting only the pet's Name for each value.
+            var query =
+                pets.GroupBy(pet => pet.Age, pet => pet.Name);
+
+            // Iterate over each IGrouping in the collection.
+            foreach (var petGroup in query)
+            {
+                // Print the key value of the IGrouping.
+                display(petGroup.Key.ToString());
+                // Iterate over each value in the 
+                // IGrouping and print the value.
+                foreach (string name in petGroup)
+                    display(name);
+
+                display();
+            }
+        }
+
+        List<Pet> petsList2;
+        private void initPets2()
+        {
+            // Create a list of pets.
+            petsList2 =
+                new List<Pet>{ new Pet { Name="Barley", Age=8.3 },
+                       new Pet { Name="Boots", Age=4.9 },
+                       new Pet { Name="Whiskers", Age=1.5 },
+                       new Pet { Name="Jimmy", Age=1.2 },
+                       new Pet { Name="Daisy", Age=4.3 } };          
+        }       
+        //groupBy 2()
+        private void button3_Click_2(object sender, EventArgs e)
+        {
+            initPets2();
+
+            // Group Pet.Age values by the Math.Floor of the age.
+            // Then project an anonymous type from each group
+            // that consists of the key, the count of the group's
+            // elements, and the minimum and maximum age in the group.
+
+            /* Args to GroupBy()
+             * 1 - Key selector
+             * 2 - Element selector
+             * 3-  Result Selector
+             */
+            var query = petsList2.GroupBy(
+                pet => Math.Floor(pet.Age),
+                pet => pet.Age,
+                (baseAge, ages) => new
+                {
+                    Key = baseAge,
+                    Count = ages.Count(),
+                    Min = ages.Min(),
+                    Max = ages.Max()
+                });
+
+            foreach(var item in query)
+            {
+                display("Age group: " + item.Key);
+                display("Number of pets in this age group: " + item.Count);
+                display("Minimum age: " + item.Min);
+                display("Maximum age: " + item.Max);
+                display();
+            }                
+        }
+
+        List<Pet> petsList3;
+        private void initPets3()
+        {
+            // Create a list of pets.
+            petsList3 =
+                new List<Pet>{ new Pet { Name="Barley", Age=8.3, Weight = 20.0 },
+                       new Pet { Name="Boots", Age=4.9, Weight = 10.0 },
+                       new Pet { Name="Whiskers", Age=1.5, Weight = 8.0 },
+                       new Pet { Name="Jimmy", Age=1.2, Weight = 10.0 },
+                       new Pet { Name="Daisy", Age=4.3, Weight = 15.0 } };
+        }
+        //GroupBy() 3
+        private void button4_Click(object sender, EventArgs e)
+        {
+            initPets3();
+
+            var query = petsList3.GroupBy(
+                pet => Math.Floor(pet.Age),
+                pet => pet.Weight,
+                (baseAge, weights) => new
+                {
+                    Key = baseAge,
+                    Count = weights.Count(),
+                    Min = weights.Min(),
+                    Max = weights.Max(),
+                    Avg = weights.Average()
+                });
+
+            foreach (var item in query)
+            {
+                display("Age group: " + item.Key);
+                display("Number of pets in this age group: " + item.Count);
+                display("Minimum weight: " + item.Min);
+                display("Maximum weight: " + item.Max);
+                display("Average weight: " + item.Avg);
+                display();
+            }
+
+            var query2 = petsList2.GroupBy(
+              pet => Math.Floor(pet.Age),
+              pet => pet.Weight);
+        }
+
+        private void display(string msg="")
+        {            
+            if (!string.IsNullOrEmpty(msg))
+                textBox1.Text += msg;
+
+            textBox1.Text += Environment.NewLine;
+        }
+
+
+        //unwanted
+        private void IntermForm1_Load(object sender, EventArgs e)
+        {
+
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void button3_Click_1(object sender, EventArgs e)
         {
 
         }
+
     }
 }
